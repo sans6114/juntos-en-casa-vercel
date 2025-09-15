5cfc3553d61f4568bb5b96ebbfd24f1f2fdd28f2
santi <santisosa244@gmail.com>
2025-09-15 14:09:34 -0300

    deployment

--- src/firebase/server.ts
+++ src/firebase/server.ts
@@ -22,12 +22,6 @@ const serviceAccount = {
 };
 
 const initApp = () => {
-  if (import.meta.env.PROD) {
-    console.info('PROD env detected. Using default service account.')
-    // Usa la configuración predeterminada en las funciones de Firebase. Debería estar ya inyectada en el servidor por Firebase.
-    return initializeApp()
-  }
-  console.info('Loading service account from env.')
   return initializeApp({
     credential: cert(serviceAccount as ServiceAccount)
   })
