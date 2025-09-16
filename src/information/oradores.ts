e3bdffa662318f45e8ef38380c128ebf86493633
santi <santisosa244@gmail.com>
2025-09-16 18:02:25 -0300

    .

--- src/information/oradores.ts
+++ src/information/oradores.ts
@@ -3,32 +3,27 @@ import type { Orador } from '../interfaces';
 export const oradores: Orador[]  = [
   {
     nombre: "Ezequiel Mangonnet",
-    imagen: "/images/orador1.jpg",
+    imagen: "/oradores/ezequielmango.webp",
     descripcion: "Descripción del Orador 1",
   },
   {
     nombre: "Florencia Mraida",
-    imagen: "/images/orador2.jpg",
+    imagen: "/oradores/flormraida.webp",
     descripcion: "Descripción del Orador 2",
-  },
-  {
-    nombre: "Natalia Spetale",
-    imagen: "/images/orador3.jpg",
-    descripcion: "Descripción del Orador 3",
   },
 {
     nombre: "Natalia Spetale",
-    imagen: "/images/orador3.jpg",
+    imagen: "/oradores/nati.webp",
     descripcion: "Descripción del Orador 3",
   },
     {
     nombre: "Ezequiel Rossini",
-    imagen: "/images/orador3.jpg",
+    imagen: "/oradores/eze.webp",
     descripcion: "Descripción del Orador 3",
   },
      {
     nombre: "Juan Pablo Sosa",
-    imagen: "/images/orador3.jpg",
+    imagen: "/oradores/juan.webp",
     descripcion: "Descripción del Orador 3",
   },
 ];
