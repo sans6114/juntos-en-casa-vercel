54b0f65ab4b9780476057056cb22bd8ca9c1cc02
santi <santisosa244@gmail.com>
2025-09-17 21:54:11 -0300

    .

--- src/information/preguntas-respuestas.ts
+++ src/information/preguntas-respuestas.ts
@@ -2,28 +2,28 @@ import type { QYA } from '../interfaces';
 
 export const preguntasRespuestas: QYA[] = [
   {
-    titulo: "¿Qué es \"Juntos en Casa\"?",
+    titulo: "¿Qué es Juntos en Casa?",
     contenido:
-      "\"Juntos en Casa\" es la conferencia de los adolescentes y jóvenes de Vida Sobrenatural. Este tiempo fue pensado para disfrutar, reír, crecer más y más en nuestra relación con Dios y adorar juntos a Jesús!!",
+      "Juntos en Casa es la Conferencia de adolescentes y jóvenes de Iglesia Vida Sobrenatural. Fue creada para crecer en unidad, ir más profundo en Dios y ser equipados para el desarrollo de nuestro propósito. También disfrutaremos de juegos, dinámicas y talleres especialmente planificados para adolescentes y jóvenes.",
+  },
+    {
+    titulo: "¿La actividad es abierta?",
+    contenido:
+      "La actividad es abierta a otras congregaciones que quieran sumarse y compartir un fin de semana de renovación Juntos en Casa.",
   },
   {
     titulo: "¿Dónde es?",
     contenido:
-      "En calle 23 Nº1665 entre 66 y 67 (zona Parque Castelli). La Plata, Bs As.",
+      "En el templo de Vida Sobrenatural: Calle 23, 1665 entre 66 y 67 (La Plata, Bs As). Zona Parque Castelli",
   },
   {
-    titulo: "¿Es gratuita o hay que pagar para participar?",
+    titulo: "¿Tiene un costo?",
     contenido:
       "La entrada a la conferencia es libre y gratuita con previa inscripción.",
   },
-  {
-    titulo: "¿Qué colectivos me llevan hasta Parque Castelli?",
-    contenido:
-      "Para ir al Parque Castelli en colectivo, podés tomar las líneas 202, 506 18A, OESTE61, OESTE62 y SUR 19, que se detienen en las cercanías del parque.",
-  },
-  {
-    titulo: "¿Puedo llevar equipo de mate y algo para comer durante la conferencia?",
-    contenido:
-      "¡Si! No hay problema, sólo que en el auditorio NO PODRÁS consumir ni mate, ni comida. La idea es aprovechar los breaks para comer algo fuera de las instalaciones del templo.",
-  }
+  //   {
+  //   titulo: "¿Puedo llevar equipo de mate y algo para comer durante la conferencia?",
+  //   contenido:
+  //     "Sí, puedes llevar equipo de mate y algo para comer. Habrá espacios designados para que puedas disfrutar de tu comida y bebida durante los recesos.",
+  // },
 ]
